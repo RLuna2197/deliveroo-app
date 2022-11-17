e387266358b51b906f3d763dce9dac40ac691dd9
roberto.luna <roberto.luna@salud.gob.sv>
2022-11-17 11:37:23 -0600

    configurando header del home

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,12 @@
-import { View, Text, SafeAreaView, Image } from 'react-native';
+import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from 'react-native';
 import React, { useLayoutEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import { UserIcon, ChevronDownIcon, MagnifyingGlassIcon, AdjustmentsVerticalIcon } from "react-native-heroicons/outline";
 
 export default function HomeScreen() {
     const navigation = useNavigation();
     
+    /* Disable default header */
     useLayoutEffect(() => {
         navigation.setOptions({
             headerShown: false,
@@ -12,16 +14,41 @@ export default function HomeScreen() {
     }, []);
     
   return (
-    <SafeAreaView>
-        <View>
-          <Text className="bg-red-300">Hcxxvbbvbvola</Text>
+    <SafeAreaView className="bg-white pt-5">
+      <Text>
+        { /*Header*/ }
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
         <Image
           source={{
             uri: "https://links.papareact.com/wru",
           }}
           className="w-7 h-7 bg-gray-300 rounded-full p-4"
         />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text>Current Location
+            <ChevronDownIcon size={20} color="#00CCBB"/>
+          </Text>
         </View>
+
+        <UserIcon size={35} color="#00CCBB"/>
+        </View>
+
+        {/*Seach*/}
+        <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+          <View className="flex-row flex-1 space-x-2 items-center bg-gray-200 p-3">
+            <MagnifyingGlassIcon color="gray" size={20}/>
+            <TextInput placeholder='Restaurants and cuisines' keyboardType='default'/>
+          </View>
+          <AdjustmentsVerticalIcon color="#00CCBB"/>
+        </View>
+        </Text>
+
+        {/*Body*/}
+        <ScrollView>
+            {/*Categories*/}
+
+        </ScrollView>
     </SafeAreaView>
     
 
